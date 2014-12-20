c921119c171187b4db605c8899aa7fc236626f3d
Narcis Mihai <narcismihai11@gmail.com>
2014-12-20 19:28:26 +0200

    Added GlobalState.java - it contains global variables.

---
+++ app/src/main/java/Utilities/GlobalState.java
@@ -0,0 +1,12 @@
+package Utilities;
+
+import android.app.Application;
+
+/**
+ * Created by Narcis11 on 20.12.2014.
+ */
+public class GlobalState {
+    public static String testString = "";
+    public static String latitude = "";
+    public static String longitude = "";
+}
