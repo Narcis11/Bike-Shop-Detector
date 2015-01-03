8d5b9092da4bcb3f5376171b094102e2318a962c
Narcis Mihai <narcismihai11@gmail.com>
2015-01-03 11:35:34 +0200

    Added an AutoCompleteTextView.

--- app/src/main/java/waldo/bike/waldo/PlacesAutoComplete.java
+++ app/src/main/java/waldo/bike/waldo/PlacesAutoComplete.java
@@ -1,7 +1,27 @@
 package waldo.bike.waldo;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.AutoCompleteTextView;
+
+import java.util.HashMap;
+
 /**
  * Created by Narcis11 on 03.01.2015.
  */
-public class PlacesAutoComplete {
+public class PlacesAutoComplete extends AutoCompleteTextView {
+
+    private String description = "description";
+    public PlacesAutoComplete(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /** Returns the place description corresponding to the selected item */
+    @Override
+    protected CharSequence convertSelectionToString(Object selectedItem) {
+        /** Each item in the autocompetetextview suggestion list is a hashmap object */
+        HashMap<String, String> hm = (HashMap<String, String>) selectedItem;
+        return hm.get(description);
+    }
+
 }
