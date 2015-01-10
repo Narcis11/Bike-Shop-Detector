c527b37b26a4cb363e65a4e24d6e5ef41239fd14
Narcis Mihai <narcismihai11@gmail.com>
2015-01-10 19:14:31 +0200

    Modified the check logic.

--- app/src/main/java/waldo/bike/form/AddShopFormActivity.java
+++ app/src/main/java/waldo/bike/form/AddShopFormActivity.java
@@ -22,12 +22,19 @@ public class AddShopFormActivity extends ActionBarActivity {
     public static final String LOG_TAG = AddShopFormActivity.class.getSimpleName();
     private static boolean mShopNameOk = false;
     private static boolean mShopWebsiteOk = true;
+    EditText mShopName;
+    EditText mShopWebsite;
+    TextView mErrorMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shop_form);
-        EditText shopName = (EditText) findViewById(R.id.new_shop_name);
-        shopName.addTextChangedListener(new TextWatcher() {
+        mShopName = (EditText) findViewById(R.id.new_shop_name);
+        mShopWebsite = (EditText) findViewById(R.id.new_shop_website);
+        mErrorMessage = (TextView) findViewById(R.id.placeholder_text);
+
+        mShopName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -40,8 +47,24 @@ public class AddShopFormActivity extends ActionBarActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                Log.i(LOG_TAG,"In afterTextChanged Listener");
                 checkShopName();
+            }
+        });
+
+        mShopWebsite.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                checkShopWebsite();
             }
         });
     }
@@ -63,31 +86,45 @@ public class AddShopFormActivity extends ActionBarActivity {
     }
 
     public void checkShopName () {
-        EditText shopName = (EditText) findViewById(R.id.new_shop_name);
-        TextView errorMessage = (TextView) findViewById(R.id.placeholder_text);
-        if (shopName.getText().toString().length() == 0) {
-            errorMessage.setText(getResources().getString(R.string.empty_shop_name));
+        if (mShopName.getText().toString().length() == 0) {
+            mErrorMessage.setText(getResources().getString(R.string.empty_shop_name));
             mShopNameOk = false;
         }
         else {
-            errorMessage.setText("");
+            mErrorMessage.setText("");
             mShopNameOk = true;
         }
     }
 
+    public void checkShopWebsite() {
+        String wwwPrefix = "www";
+        //mShopWebsite.toString().indexOf(wwwPrefix) == 0 ||
+        if (mShopWebsite.getText().toString().length() > 0) {
+            if (!Patterns.WEB_URL.matcher(mShopWebsite.getText()).matches()) {
+                Log.i(LOG_TAG, "Invalid web address");
+                mErrorMessage.setText(getResources().getString(R.string.invalid_url));
+                mShopWebsiteOk = false;
+            } else {
+                mErrorMessage.setText("");
+                mShopWebsiteOk = true;
+            }
+        }
+        else {
+            mErrorMessage.setText("");
+            mShopWebsiteOk = true;
+        }
+    }
 
     public void addShop(View v) {
-        EditText shopName = (EditText) findViewById(R.id.new_shop_name);
-
-        EditText shopWebsiteAddress = (EditText) findViewById(R.id.new_shop_website);
-        TextView errorMessage = (TextView) findViewById(R.id.placeholder_text);
-        String wwwPrefix = "www";
-        if (shopName.getText().toString().length() == 0) {
-            errorMessage.setText(getResources().getString(R.string.empty_shop_name));
+        if (mShopWebsite.getText().toString().length() == 0) {
+            //this field is not mandatory
+            mShopWebsiteOk = true;
         }
-/*        else if (shopWebsiteAddress.toString().indexOf(wwwPrefix) == 0 || !Patterns.WEB_URL.matcher(shopWebsiteAddress.getText()).matches()) {
-            Log.i(LOG_TAG,"Invalid web address");
-            errorMessage.setText(getResources().getString(R.string.invalid_url));
-        }*/
+        if (mShopWebsiteOk && mShopNameOk) {
+            Log.i(LOG_TAG,"OK to submit form");
+        }
+        else {
+            mErrorMessage.setText(getResources().getString(R.string.invalid_form));
+        }
     }
 }
