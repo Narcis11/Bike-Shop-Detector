0650461248168f8d67c3681a84eb7430cf3d514e
Narcis Mihai <narcismihai11@gmail.com>
2015-01-10 16:31:37 +0200

    "Add shop" button added.

--- app/src/main/java/waldo/bike/form/AddShopFormActivity.java
+++ app/src/main/java/waldo/bike/form/AddShopFormActivity.java
@@ -2,13 +2,16 @@ package waldo.bike.form;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import waldo.bike.waldo.R;
 
 public class AddShopFormActivity extends ActionBarActivity {
 
+    public static final String LOG_TAG = AddShopFormActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +32,9 @@ public class AddShopFormActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addShop(View v) {
+        Log.i(LOG_TAG,"Pressed add shop button");
     }
 }
