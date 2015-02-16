c690d9ee367a14e73d9cb6dd1ba85133d68a21f8
Narcis Mihai <narcismihai11@gmail.com>
2015-02-16 17:56:05 +0200

    Push notifications are now working.

--- app/src/main/java/waldo/bike/bikeshops/BikeShopsDetector.java
+++ app/src/main/java/waldo/bike/bikeshops/BikeShopsDetector.java
@@ -1,10 +1,14 @@
 package waldo.bike.bikeshops;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
@@ -30,12 +34,15 @@ public class BikeShopsDetector extends Application {
         APP_TRACKER, // Tracker used only in this app.
     }
     private static final String PROPERTY_ID = "UA-55296895-2";
+    private static final String PARSE_APP_ID = "uD0QY3adOHScxd64i1dTOekQymn9aZVrQTbc9N5N";
+    private static final String PARSE_CLIENT_KEY = "VuRbrENiboj1i9tHg73xSa3FT58spKnMaSnH7O38";
+    private static final String LOG_TAG = BikeShopsDetector.class.getSimpleName();
     HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Parse.initialize(this, "uD0QY3adOHScxd64i1dTOekQymn9aZVrQTbc9N5N", "VuRbrENiboj1i9tHg73xSa3FT58spKnMaSnH7O38");
+        Parse.initialize(this, PARSE_APP_ID, PARSE_CLIENT_KEY);
         authConfig =
                 new TwitterAuthConfig(Constants.CONSUMER_KEY,
                         Constants.CONSUMER_SECRET);
@@ -45,6 +52,17 @@ public class BikeShopsDetector extends Application {
         catch (Exception e) {
 
         }
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d(LOG_TAG, "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e(LOG_TAG, "failed to subscribe for push", e);
+                }
+            }
+        });
     }
     synchronized Tracker getTracker(TrackerName trackerId) {
         if (!mTrackers.containsKey(trackerId)) {
