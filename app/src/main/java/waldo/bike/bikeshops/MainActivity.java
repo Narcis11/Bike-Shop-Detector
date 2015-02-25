33621810598b770d605f99329df4a85082004d4c
Narcis Mihai <narcismihai11@gmail.com>
2015-02-25 15:00:02 +0200

    Fixed #1: Back button not working in main activity.

--- app/src/main/java/waldo/bike/bikeshops/MainActivity.java
+++ app/src/main/java/waldo/bike/bikeshops/MainActivity.java
@@ -386,8 +386,7 @@ public class MainActivity extends Activity implements
 
              @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        this.finish();
+        //we don't do anything when the back button is pressed.
     }
 
     //called when like/unlike or (un)follow action is executed
